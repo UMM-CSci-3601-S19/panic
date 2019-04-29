6507dd4f40cc14a70f3e017c20ae5d361406d9d4
Avery Koranda <koran023@morris.umn.edu>
2019-04-29 12:54:17 -0500

    Remove unused code
    Referenced ChatController which is an old, outdated class from iteration 2.
    
    #2, #12

--- server/src/main/java/umm3601/chat/ChatRequestHandler.java
+++ server/src/main/java/umm3601/chat/ChatRequestHandler.java
@@ -16,36 +16,6 @@ import com.auth0.jwt.algorithms.Algorithm;
 
 public class ChatRequestHandler {
 
-  private final ChatController chatController;
-
-  public ChatRequestHandler(ChatController chatController) {
-    this.chatController = chatController;
-  }
-
-  /**
-   * Method called from Server when the 'api/chats/:id' endpoint is received.
-   * Get a JSON response with the chat found by provided id.
-   */
-  public String getChatJSON(Request req, Response res) {
-    res.type("application/json");
-    String id = req.params("id");
-    String chat;
-    try {
-      chat = chatController.getChat(id);
-    } catch (IllegalArgumentException iae) {
-      res.status(400);
-      res.body("The requested chat id " + id + " wasn't a legal Mongo Object ID.");
-      return "";
-    }
-    if (chat != null) {
-      return chat;
-    } else {
-      res.status(404);
-      res.body("The request chat with id " + id + " was not found");
-      return "";
-    }
-  }
-
   public Token authenticateChatUser(Request req, Response res) {
     res.type("application/json");
 
