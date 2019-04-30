03ee6c7e0c6be1deaf5520c87f2da033d1c23397
teklu009 <teklu009@morris.umn.edu>
2019-04-30 12:13:19 -0500

    change styleUrls from css to scss

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,7 +5,7 @@ import {ProfileService} from "./users/profile.service";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
   providers: [AppService]
 })
 export class AppComponent {
