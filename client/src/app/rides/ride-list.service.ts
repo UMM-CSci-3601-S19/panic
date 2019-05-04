f0b4930f52d3779987a52fafd89b474c0ba3983f
teklu009 <teklu009@morris.umn.edu>
2019-05-04 13:13:57 -0500

    Created DriveRide in rideListService for connecting drive ride to the server

--- client/src/app/rides/ride-list.service.ts
+++ client/src/app/rides/ride-list.service.ts
@@ -6,6 +6,7 @@ import {Observable} from 'rxjs/Observable';
 import {Ride} from './ride';
 import {environment} from '../../environments/environment';
 import {requestRideObject} from "./requestRideObject";
+import {driveRideObject} from "./driveRideObject";
 import {leaveRideObject} from "./leaveRideObject";
 import {Subject} from "rxjs/Subject";
 import {tap} from "rxjs/operators";
@@ -53,6 +54,26 @@ export class RideListService {
       );
   }
 
+  driveRide(editedRide: driveRideObject) {
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        // We're sending JSON
+        'Content-Type': 'application/json'
+      }),
+      // But we're getting a simple (text) string in response
+      // The server sends the hex version of the new ride back
+      // so we know how to find/access that user again later.
+      responseType: 'text' as 'json'
+    };
+
+    return this.http.post<string>(this.rideUrl + '/drive', editedRide, httpOptions)
+      .pipe(
+        tap(() => {
+          this._refreshNeeded$.next();
+        })
+      );
+  }
 
 
 
