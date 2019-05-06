2eb31a67e30e744b08b72d2e747491632a1bd4df
Avery Koranda <koran023@morris.umn.edu>
2019-05-06 11:57:31 -0500

    Fix test issue caused by ChatService
    - in ChatService, import credentials file using '* as' syntax
    - in RideListComponent spec file, import HttpClientTestingModule, include ChatService as provider
    - in RideListComponent, inject ChatService before the MatDialog (order matters)
    
    #2 #12

--- client/src/app/chat/chat-service.ts
+++ client/src/app/chat/chat-service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
-import Credentials from '../../../../credentials.json';
+import * as Credentials from '../../../../credentials.json';
 
 import {Observable} from 'rxjs/Observable';
 
