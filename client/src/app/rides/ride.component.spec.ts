5c5f6eb27b9eb506ded8bac9bac2c2251a4e3a76
Joe Deglman <deglm006@morris.umn.edu>
2019-05-13 19:47:10 -0500

    Update to match master

--- client/src/app/rides/ride.component.spec.ts
+++ client/src/app/rides/ride.component.spec.ts
@@ -12,18 +12,10 @@ import {ProfileService} from "../users/profile.service";
 
 describe('RideComponent', () => {
   let component: RideComponent;
-  let rideList: RideListComponent;
-  let componentFixture: ComponentFixture<RideComponent>;
   let fixture: ComponentFixture<RideComponent>;
 
-  let rideListServiceStub: {
-    getRides: () => Observable<Ride[]>,
-    refreshNeeded$: Subject<void>,
-    requestJoinRide: () => Observable<joinRideObject>
+  let componentServiceStub: {
   };
-
-  let linkDes;
-  let routerLinks;
   let rideStub: Ride = {
     owner: '',
     ownerID: '',
@@ -39,91 +31,6 @@ describe('RideComponent', () => {
     pendingPassengerIds: [],
     pendingPassengerNames: []
   };
-
-
-  let componentServiceStub: {
-  };
-  beforeEach(() => {
-    // stub RideService for test purposes
-    rideListServiceStub = {
-      getRides: () => Observable.of([
-        {
-          _id: {$oid: 'chris_id'},
-          owner: 'Chris',
-          ownerID: "001",
-          driver: 'Chris',
-          driverID: 'chris_id',
-          notes: 'These are Chris\'s ride notes',
-          seatsAvailable: 3,
-          origin: 'UMM',
-          destination: 'Willie\'s',
-          departureDate: '3/6/2019',
-          departureTime: '10:00:00',
-          nonSmoking: true,
-          roundTrip: true,
-          pendingPassengerIds: [],
-          pendingPassengerNames: [],
-          passengerIds: [],
-          passengerNames: []
-        },
-        {
-          _id: {$oid: 'dennis_id'},
-          owner: 'Dennis',
-          ownerID: "002",
-          notes: 'These are Dennis\'s ride notes',
-          seatsAvailable: -1,
-          origin: 'Caribou Coffee',
-          destination: 'Minneapolis, MN',
-          departureDate: '8/15/2018',
-          departureTime: '11:30:00',
-          nonSmoking: true,
-          roundTrip: true,
-          pendingPassengerIds: [],
-          pendingPassengerNames: [],
-          passengerIds: ["002"],
-          passengerNames: ['Dennis']
-        },
-        {
-          _id: {$oid: 'agatha_id'},
-          owner: 'Agatha',
-          ownerID: "003",
-          driver: 'Agatha',
-          driverID: 'agatha_id',
-          notes: 'These are Agatha\'s ride notes',
-          seatsAvailable: 3,
-          origin: 'UMM',
-          destination: 'Fergus Falls, MN',
-          departureDate: '3/30/2019',
-          departureTime: '16:30:00',
-          nonSmoking: false,
-          roundTrip: false,
-          pendingPassengerIds: [],
-          pendingPassengerNames: [],
-          passengerIds: [],
-          passengerNames: []
-        }
-      ]),
-      requestJoinRide: () => Observable.of({
-        rideId: 'chris_id',
-        pendingPassengerId: "002",
-        pendingPassengerName: 'Dennis'
-      }),
-      refreshNeeded$: new Subject<void>()
-    };
-
-    TestBed.configureTestingModule({
-      imports: [CustomModule, HttpClientTestingModule],
-      declarations: [RideListComponent,RouterLinkDirectiveStub,RideComponent, ChatComponent],
-      providers: [
-        {provide: RideListService, useValue: rideListServiceStub},
-        ChatService,
-        UserService,
-        RideComponent
-      ]
-    });
-  });
-
-
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -194,51 +101,4 @@ describe('RideComponent', () => {
   it('the client parses 09:59 time to 9:59 AM', () => {
     expect(component.hourParse("09:59")).toBe("9:59 AM");
   });
-
-
-
-  /*it('contains all the rides', () => {
-    expect(component.rides.length).toBe(3);
-  });
-
-  it('request a ride', () => {
-    expect(component.rides.some((ride: Ride) => ride.pendingPassengerIds[0] === "002")).toBe(false);
-    expect(component.rides.some((ride: Ride) => ride.pendingPassengerNames[0] === 'Dennis')).toBe(false);
-    component.requestJoinRide('chris_id', "002", 'Dennis');
-    rideList.refreshRides().subscribe(() => {
-
-      expect(component.rides.some((ride: Ride) => ride.pendingPassengerIds[0] === "002")).toBe(true);
-      expect(component.rides.some((ride: Ride) => ride.pendingPassengerNames[0] === 'Dennis')).toBe(true);
-    });
-  });
-
-  it('accept a ride', () =>{
-    expect(component.rides.some((ride: Ride) => ride.pendingPassengerIds[0] === "002")).toBe(false);
-    expect(component.rides.some((ride: Ride) => ride.pendingPassengerNames[0] === 'Dennis')).toBe(false);
-    expect(component.rides.some((ride: Ride) => ride.passengerIds[0] === "002")).toBe(false);
-    expect(component.rides.some((ride: Ride) => ride.passengerNames[0] === 'Dennis')).toBe(false);
-    component.requestJoinRide('chris_id', "002", 'Dennis');
-    component.approveJoinRide('chris_id', "002", 'Dennis');
-    rideList.refreshRides().subscribe(() => {
-      expect(component.rides.some((ride: Ride) => ride.pendingPassengerIds[0] === "002")).toBe(false);
-      expect(component.rides.some((ride: Ride) => ride.pendingPassengerIds[0] === 'Dennis')).toBe(false);
-      expect(component.rides.some((ride: Ride) => ride.passengerIds[0] === "002")).toBe(true);
-      expect(component.rides.some((ride: Ride) => ride.passengerNames[0] === 'Dennis')).toBe(true);
-    });
-  });
-
-  it('decline a ride', () => {
-    expect(component.rides.some((ride: Ride) => ride.pendingPassengerIds[0] === "002")).toBe(false);
-    expect(component.rides.some((ride: Ride) => ride.pendingPassengerNames[0] === 'Dennis')).toBe(false);
-    expect(component.rides.some((ride: Ride) => ride.passengerIds[0] === "002")).toBe(false);
-    expect(component.rides.some((ride: Ride) => ride.passengerNames.length[0] === 'Dennis')).toBe(false);
-    component.requestJoinRide('chris_id', "002", 'Dennis');
-    component.declineJoinRide('chris_id', "002", 'Dennis');
-    rideList.refreshRides().subscribe(() => {
-      expect(component.rides.some((ride: Ride) => ride.pendingPassengerIds[0] === "002")).toBe(false);
-      expect(component.rides.some((ride: Ride) => ride.pendingPassengerNames[0] === 'Dennis')).toBe(false);
-      expect(component.rides.some((ride: Ride) => ride.passengerIds[0] === "002")).toBe(false);
-      expect(component.rides.some((ride: Ride) => ride.passengerNames[0] === 'Dennis')).toBe(false);
-    });
-  });*/
 });
